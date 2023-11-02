dcf59f41368d1fbdca3aaeec117ae03e6a7fc173
vismo_dev01~ <vismodeb2000@gmail.com>
2023-11-02 08:20:46 +0600

    owl Carousel Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,22 @@
-// $(document).ready(function(){
-//     // all testmonial gallery
-//     $('.main_mercedes_contain').owlCarousel({
-//         loop:true,
-//         nav:false,
-//         margin: 40,
-//         center:true,
-//         autoplay:true,
-//         responsive:{
-//             0:{
-//                 items:2
-//             },
-//             575:{
-//                 items:3
-//             },
-//             767:{
-//                 items:4
-//             },
-//             1000:{
-//                 items:6
-//             }
-//         }
-//     });
-// });
-
 $(document).ready(function(){
-    $('.main_mercedes_contain').owlCarousel({
-      loop:true,
-      nav:false,
-      margin:40,
-      center:true,
-      autoplay:true,
-      autoplayTimeout:2000,
-      responsive:{
-        0:{
-          items:2
-        },
-        575:{
-          items:3
-        },
-        767:{
-          items:3
-        },
-        991:{
-          items:6
-        }
+  // main mercedes contain
+  $('.main_mercedes_contain').owlCarousel({
+    loop:true,
+    nav:false,
+    margin:30,
+    center: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+      0:{
+        items:2
+      },
+      575:{
+        items:3
+      },
+      767:{
+        items:6
       }
-    });
+    }
   });
+});
